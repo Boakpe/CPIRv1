0bf76cb3fa3107773f05b2ba57cad0389ac680b0
Breno <breno.cpedroso@gmail.com>
2023-09-19 01:41:00 -0300

    Update folder structure

--- texttospeech.py
+++ texttospeech.py
@@ -1,18 +1,14 @@
 import gtts
-from pydub import AudioSegment
-from pydub.playback import play
+from playsound import playsound
 
-# Função para falar e reproduzir o áudio
-def speak_and_play(text):
-    tts = gtts.gTTS(text, lang="pt-br")
-    audio = AudioSegment.from_mp3(tts.get_urls()[0])
-    play(audio)
+tts = gtts.gTTS("Por favor mostre a etiqueta para identificação do destinátario", lang="pt-br")
+tts.save("audios/cumprimentos.mp3")
+playsound("audios/cumprimentos.mp3")
 
-# Mensagem 1
-speak_and_play("Por favor mostre a etiqueta para identificação do destinatário")
+tts = gtts.gTTS("Houve um erro na leitura da etiqueta, por favor tente novamente", lang="pt-br")
+tts.save("audios/erro.mp3")
+playsound("audios/erro.mp3")
 
-# Mensagem 2
-speak_and_play("Houve um erro na leitura da etiqueta, por favor tente novamente")
-
-# Mensagem 3
-speak_and_play("A etiqueta foi lida com sucesso, por favor coloque o objeto na caixa")
+tts = gtts.gTTS("A etiqueta foi lida com sucesso, por favor coloque o objeto na caixa", lang="pt-br")
+tts.save("audios/sucesso.mp3")
+playsound("audios/sucesso.mp3")
