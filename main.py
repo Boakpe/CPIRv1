6fd9376c169ad4f4919648ffbb5ce95769e2ae29
CaioNoro <caio.noronha@estudante.ufla.br>
2023-09-22 01:07:52 -0300

    Update on text reading (not working yet)

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import ticketidf as tidf
 while True:
     try: 
         pmidf.camera()
-        tts.cumprimentos()
-        while not tidf.camera:
-            tts.erro()
+        #tts.cumprimentos()
+        #while not tidf.camera:
+            #tts.erro()
             # Se passar de 4 tentativas de leitura da etiqueta, o programa é encerrado
             # É salvo um arquivo .json com a data e hora da última tentativa
-        tts.sucesso()
+        #tts.sucesso()
         # É salvo um arquivo .json com a data e hora e o nome do destinatário
         break
     except:
--- postmanidf.py
+++ postmanidf.py
@@ -17,6 +17,7 @@ def camera():
     camera = cv2.VideoCapture(0)
 
     carteiro_index = 0
+    flagCarteiro = False
     while True:
         # Grab the webcamera's image.
         ret, image = camera.read()
@@ -32,62 +33,42 @@ def camera():
 
         # Normalize the image array
         image = (image / 127.5) - 1
-
-        # Predicts the model
-        prediction = model.predict(image)
-        index = np.argmax(prediction)
-        class_name = class_names[index]
-        confidence_score = prediction[0][index]
-
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-        if class_name[2:].strip() == "Carteiro":
-            carteiro_index += 1
-        else:
-            carteiro_index = 0
-
-        if carteiro_index == 10:
-            print("Carteiro detectado!")
-            
-            return True
-
-        # Listen to the keyboard for presses.
-        keyboard_input = cv2.waitKey(200)
-
-        # 27 is the ASCII for the esc key on your keyboard.
-        if keyboard_input == 27:
-            break
         
-    # Caso tenha achado o carteiro, procura pelo pacote
-    if carteiro_index == 10:
-        while True:            
-            # Grab the webcamera's image.
-            ret, image = camera.read()
-
-            # Resize the raw image into (224-height,224-width) pixels
-            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-            # Show the image in a window
-            cv2.imshow("Webcam Image", image)
-
-            # Make the image a numpy array and reshape it to the models input shape.
-            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-            # Normalize the image array
-            image = (image / 127.5) - 1
-            
-            # Insira aqui a mesma logica anterior para identificar o pacote
-            
+        if flagCarteiro:     
             text = pytesseract.image_to_string(image)
             print(text)
-    
-    
+
+        else:
+            # Predicts the model
+            prediction = model.predict(image)
+            index = np.argmax(prediction)
+            class_name = class_names[index]
+            confidence_score = prediction[0][index]
+
+            # Print prediction and confidence score
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+            # Print prediction and confidence score
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+            if class_name[2:].strip() == "Carteiro":
+                carteiro_index += 1
+            else:
+                carteiro_index = 0
+
+            if carteiro_index == 10:
+                print("Carteiro detectado!")
+                flagCarteiro = True
+                #return True
+
+            # Listen to the keyboard for presses.
+            keyboard_input = cv2.waitKey(200)
+
+            # 27 is the ASCII for the esc key on your keyboard.
+            if keyboard_input == 27:
+                break  
 
     camera.release()
     cv2.destroyAllWindows()
